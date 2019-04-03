3d5a4278029add3c328de30d3713fe6d6f113f8b
bluebaney10 <bluebaney10@gmail.com>
2019-04-03 16:58:40 +0700

    add splass screen

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 /** @format */
 
 import {AppRegistry} from 'react-native';
-//import App from './App/components/App';
-//import App from './App/components/AppProps';
-//import App from './App/components/AppFlex';
-//import App from './App/ProgressContainner.js'
 
 //import App from './App/components/AppTemplate';
-import App from './App/components/AppSplashScreen';
+//import App from './App/components/AppSplashScreen';
+import App from './App/components/AppNav';
 
 
 import {name as appName} from './app.json';
